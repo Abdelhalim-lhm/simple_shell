4a4875394b13681186c75af123d599ad13266a16
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-09-21 19:27:14 -0700

    fixing betty

--- path.c
+++ path.c
@@ -6,22 +6,18 @@
  */
 int path(char *cmd)
 {
-	char *argv[MAX_CMD_LENGTH / 2];
-	int i = 0;
-	char *token;
-	int result, cmd_len;
-	char *full_path;
+	char *argv[MAX_CMD_LENGTH / 2], *token, *full_path;
+	int result, cmd_len, i = 0;
 
-       	while (*cmd == ' ')
- 	{
- 		cmd++;
- 	}
- 	cmd_len = _strlen(cmd);
-	
+	while (*cmd == ' ')
+	{
+		cmd++;
+	}
+	cmd_len = _strlen(cmd);
 	while (cmd_len > 0 && cmd[cmd_len - 1] == ' ')
 	{
-        	cmd[cmd_len - 1] = '\0';
-       	 cmd_len--;
+		cmd[cmd_len - 1] = '\0';
+		cmd_len--;
 	}
 	token = strtok(cmd, " \t");
 	if (token == NULL)
@@ -35,7 +31,6 @@ int path(char *cmd)
 		token = strtok(NULL, " \t");
 	}
 	argv[i] = NULL;
-
 	if (cmd[0] == '/')
 	{
 		result = execute_command(cmd, argv);
@@ -48,7 +43,6 @@ int path(char *cmd)
 		print("hsh:%s: command not found\n", argv[0]);
 		return (127);
 	}
-
 	result = execute_command(full_path, argv);
 	free(full_path);
 	return (result);
