22f15ee430b7894408251cd0fab27590a8128315
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-09-21 19:15:25 -0700

    fixing output of a command with spaces

--- path.c
+++ path.c
@@ -1,9 +1,7 @@
 #include "shell.h"
-
 /**
  * path - Execute a command based on its path or name.
  * @cmd: The command to execute.
- *
  * Return: 0 on success, status on failure.
  */
 int path(char *cmd)
@@ -11,9 +9,20 @@ int path(char *cmd)
 	char *argv[MAX_CMD_LENGTH / 2];
 	int i = 0;
 	char *token;
-	int result;
+	int result, cmd_len;
 	char *full_path;
 
+       	while (*cmd == ' ')
+ 	{
+ 		cmd++;
+ 	}
+ 	cmd_len = _strlen(cmd);
+	
+	while (cmd_len > 0 && cmd[cmd_len - 1] == ' ')
+	{
+        	cmd[cmd_len - 1] = '\0';
+       	 cmd_len--;
+	}
 	token = strtok(cmd, " \t");
 	if (token == NULL)
 	{
